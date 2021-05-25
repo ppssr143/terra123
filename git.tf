1d25d104b881bf51381d34566988d9e9e3ad2492
EC2 Default User <ec2-user@ip-172-31-19-198.us-east-2.compute.internal>
2021-05-25 06:14:02 +0000

    adkkdfk

---
+++ git.tf
@@ -0,0 +1,27 @@
+provider "aws"{
+        region = "us-east-2"
+}
+resource "aws_instance" "web" {
+ # ami           = "ami-08e6b682a466887dd"
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+  #key_name      = "terraform"
+  key_name      = "terraform"
+  user_data     = <<-EOF
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt install apache2 -y
+                sudo systemctl enable apache2.service
+                sudo systemctl start apache2.service
+                sudo apt-get install git -y
+                git clone https://github.com/ppssr143/terra123.git /home/ubuntu/test/
+                sudo rm -rf /var/www/html/index.html
+                sudo cp /home/ubuntu/test/index.html /var/www/html/
+                EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
